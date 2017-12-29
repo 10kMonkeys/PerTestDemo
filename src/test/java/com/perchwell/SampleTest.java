7bf7ec4174442b3920682e351205bde017bb3349
SiarheiStsepanenka <lotosss48@gmail.com>
2017-12-29 15:06:42 +0300

    Add keeping travis alive

--- src/test/java/com/perchwell/SampleTest.java
+++ src/test/java/com/perchwell/SampleTest.java
@@ -1,11 +1,16 @@
 package com.perchwell;
 
+import com.perchwell.helpers.TravisAlive;
 import com.perchwell.steps.*;
 import net.serenitybdd.junit.runners.SerenityRunner;
 import net.thucydides.core.annotations.Managed;
 import net.thucydides.core.annotations.Steps;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.runner.RunWith;
 import org.openqa.selenium.WebDriver;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 
 @RunWith(SerenityRunner.class)
@@ -39,8 +44,16 @@ public class SampleTest {
 	TagsSteps tagsSteps;
 
 	@Steps
-SearchSteps searchSteps;
+    SearchSteps searchSteps;
 
 	@Steps
-MapSteps mapSteps;
+    MapSteps mapSteps;
+
+    static TimerTask task = new TravisAlive();
+    static Timer timer = new Timer(true);
+
+    @BeforeAll
+    public static void keepTravisAlive(){
+        timer.schedule(task, 15000, 540000);
+    }
 }
