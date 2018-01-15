3a56d4e3ebf2bad80ea432798ea983882dd2ad4b
IrinaKolodina <iramrz@gmail.com>
2018-01-15 22:01:08 +0300

    for conflict resolution

--- src/test/java/com/perchwell/SampleTest.java
+++ src/test/java/com/perchwell/SampleTest.java
@@ -1,5 +1,6 @@
 package com.perchwell;
 
+
 import com.perchwell.helpers.TravisAlive;
 import com.perchwell.steps.*;
 import net.serenitybdd.junit.runners.SerenityRunner;
@@ -17,58 +18,62 @@ import java.util.TimerTask;
 @RunWith(SerenityRunner.class)
 public abstract class SampleTest {
 
-@Managed
-WebDriver driver;
+    @Managed
+    WebDriver driver;
 
-@Steps
-protected  CompareSteps compareSteps;
+    @Steps
+    protected  CompareSteps compareSteps;
 
-@Steps
-protected  OpenedBuildingSteps openedBuildingSteps;
+    @Steps
+    protected  OpenedBuildingSteps openedBuildingSteps;
 
-@Steps
-protected LoginSteps loginSteps;
+    @Steps
+    protected LoginSteps loginSteps;
 
-@Steps
-protected ResetPasswordSteps resetPasswordSteps;
+    @Steps
+    protected ResetPasswordSteps resetPasswordSteps;
 
-@Steps
-protected PerchwellSteps perchwellSteps;
+    @Steps
+    protected PerchwellSteps perchwellSteps;
 
-@Steps
-protected PerchPopupSteps perchPopupSteps;
+    @Steps
+    protected PerchPopupSteps perchPopupSteps;
 
-@Steps
-protected AccountSteps accountSteps;
+    @Steps
+    protected AccountSteps accountSteps;
 
-@Steps
-protected ClientSteps clientSteps;
+    @Steps
+    protected ClientSteps clientSteps;
 
-@Steps
-protected TagsSteps tagsSteps;
+    @Steps
+    protected TagsSteps tagsSteps;
 
-@Steps
-protected SearchSteps searchSteps;
+    @Steps
+    protected SearchSteps searchSteps;
 
-@Steps
-protected MapSteps mapSteps;
+    @Steps
+    protected MapSteps mapSteps;
 
-@Steps
-protected DiscussionSteps discussionSteps;
+    @Steps
+    protected DiscussionSteps discussionSteps;
 
-@Steps
-protected SelectResultSteps selectResultStep;
+    @Steps
+    protected SelectResultSteps selectResultStep;
+
+    @Steps
+    protected DiscussionsListSteps discussionsListSteps;
+
 
     static TimerTask task = new TravisAlive();
     static Timer timer;
 
 
-@BeforeClass
-public static void keepTravisAlive(){
-    if(timer == null){
-        TimerTask task = new TravisAlive();
-        timer = new Timer(true);
-        timer.schedule(task, 15000, 10000);
+    @BeforeClass
+    public static void keepTravisAlive(){
+        if(timer == null){
+            TimerTask task = new TravisAlive();
+            timer = new Timer(true);
+            timer.schedule(task, 15000, 10000);
+        }
     }
-}
 }
