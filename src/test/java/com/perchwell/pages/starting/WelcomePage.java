30e470486cdf41f7b37073a3e19e0bcb0a8009ef
Ivan Tkachou <>
2018-02-22 11:54:40 +0300

    add fix TC#3

--- src/test/java/com/perchwell/pages/starting/WelcomePage.java
+++ src/test/java/com/perchwell/pages/starting/WelcomePage.java
@@ -7,19 +7,17 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import com.perchwell.pages.base.BasePage;
 
-
-
 public class WelcomePage extends BasePage {
 
     public WelcomePage(WebDriver driver) {
         super (driver);
     }
 
-
     @iOSXCUITFindBy(accessibility = "LOG IN")
     private WebElement loginButton;
 
-    @iOSXCUITFindBy(accessibility = "CONNECT WITH")
+    @iOSXCUITFindBy(xpath = "//XCUIElementTypeApplication[@name=\"Perchwell\"]/XCUIElementTypeWindow[1]/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther[3]/XCUIElementTypeOther[2]/XCUIElementTypeButton")
+    		//accessibility = "CONNECT WITH"
     private WebElement connectWithButton;
 
     @iOSXCUITFindBy(accessibility = "“Perchwell Staging” Wants to Use “google.com” to Sign In")
@@ -28,15 +26,17 @@ public class WelcomePage extends BasePage {
 	@iOSXCUITFindBy(accessibility = "Continue")
 	private WebElement continueButton;
 
-@iOSXCUITFindBy(accessibility = "FORGOT PASSWORD")
-private WebElement forgotPassword;
+	@iOSXCUITFindBy(accessibility = "FORGOT PASSWORD")
+	private WebElement forgotPassword;
 
     public void clickConnectWith() { element(connectWithButton).click(); }
-public void clickForgotPassword() { element(forgotPassword).click(); }
+
+	public void clickForgotPassword() { element(forgotPassword).click(); }
 
     public void clickLoginButton(){
         element(loginButton).click();
     }
+
 	public void clickContinueButton(){
 		element(continueButton).click();
 	}
