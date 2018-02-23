f97ac7160d314e0590b7b5a91ee2d8d437bd355e
ICX\iryna.kalodzina <iryna.kalodzina@itechart-group.com>
2018-02-23 16:33:46 +0300

    propertties for ios\android

--- src/test/java/com/perchwell/pages/starting/LoginPage.java
+++ src/test/java/com/perchwell/pages/starting/LoginPage.java
@@ -1,6 +1,8 @@
 package com.perchwell.pages.starting;
 
 import com.perchwell.pages.base.BasePage;
+import io.appium.java_client.pagefactory.AndroidBy;
+import io.appium.java_client.pagefactory.AndroidFindBy;
 import io.appium.java_client.pagefactory.iOSXCUITFindBy;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -9,13 +11,15 @@ import org.openqa.selenium.WebElement;
 public class LoginPage extends BasePage {
 
     public LoginPage(WebDriver driver){ super (driver);}
-
+  @AndroidFindBy(id ="com.perchwell.perchwellapp:id/email" )
     @iOSXCUITFindBy(iOSClassChain = "**/XCUIElementTypeTextField")
     private WebElement emailTextBox;
 
+    @AndroidFindBy(id ="com.perchwell.perchwellapp:id/password" )
     @iOSXCUITFindBy(iOSClassChain = "**/XCUIElementTypeSecureTextField")
     private WebElement passwordField;
 
+    @AndroidFindBy(id ="com.perchwell.perchwellapp:id/log_in" )
     @iOSXCUITFindBy(accessibility = "LOG IN")
     private WebElement loginButton;
 
