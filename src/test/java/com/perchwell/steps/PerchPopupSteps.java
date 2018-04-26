5d9fa157e48e6acb0045e6ec5706c085b6d6b03a
Ivan Tkachou <>
2018-04-26 16:03:22 +0300

    refactoring PerchPopupPage and PerchPopupSteps

--- src/test/java/com/perchwell/steps/PerchPopupSteps.java
+++ src/test/java/com/perchwell/steps/PerchPopupSteps.java
@@ -3,21 +3,20 @@ package com.perchwell.steps;
 import net.thucydides.core.annotations.Step;
 import net.thucydides.core.steps.ScenarioSteps;
 import com.perchwell.pages.perchwell.PerchPopupPage;
-import org.junit.Assert;
 
 public class PerchPopupSteps extends ScenarioSteps {
+
     PerchPopupPage onPage;
 
     @Step
     public void clickNotNowButton () {
-        onPage.clickNotNowButton();
+        onPage.clickOnNotNowButton();
     }
 
-
+    /* Not use in the new version
     @Step
     public void shouldSeePopupText() throws Exception {
-        Assert.assertTrue(onPage.popupTextIsDispalyed());
+        Assert.assertTrue(onPage.isPopupTextDispalyed());
     }
-
-
+    */
 }
