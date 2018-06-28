0b900384aff01b68b819cedaa872f8da625ba5c7
10kMonkeys <aliakseiyarmalchuk@gmail.com>
2018-06-28 15:15:53 +0300

    Code Refactoring.

--- src/test/java/com/perchwell/helpers/Helper.java
+++ src/test/java/com/perchwell/helpers/Helper.java
@@ -1,8 +1,10 @@
 package com.perchwell.helpers;
 
+import com.opera.core.systems.mobile.MobileDevices;
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileBy;
 import io.appium.java_client.TouchAction;
+import io.appium.java_client.ios.IOSDriver;
 import net.thucydides.core.util.EnvironmentVariables;
 import net.thucydides.core.util.SystemEnvironmentVariables;
 import net.thucydides.core.webdriver.WebDriverFacade;
@@ -183,6 +185,6 @@ public abstract class Helper {
         WebDriver webDriver = webDriverFacade.getProxiedDriver();
         AppiumDriver appiumDriver = (AppiumDriver) webDriver;
 
-        appiumDriver.hideKeyboard();
+        appiumDriver.navigate().back();
     }
 }
