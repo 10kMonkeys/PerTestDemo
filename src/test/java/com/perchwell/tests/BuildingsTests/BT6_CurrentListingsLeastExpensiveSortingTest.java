bcd9507cc902c26e66ed0a78a329e6fcdf2b0306
YauheniShautsou <shevtsovea97@gmail.com>
2018-09-28 10:59:50 +0300

    BT6(Current listings - Least expensive sorting) test implemented. Code refactoring.

--- src/test/java/com/perchwell/tests/BuildingsTests/BT6_CurrentListingsLeastExpensiveSortingTest.java
+++ src/test/java/com/perchwell/tests/BuildingsTests/BT6_CurrentListingsLeastExpensiveSortingTest.java
@@ -2,16 +2,15 @@ package com.perchwell.tests.BuildingsTests;
 
 import com.perchwell.SampleTest;
 import com.perchwell.data.Addresses;
-import com.perchwell.data.Filters;
 import com.perchwell.entity.AppProperties;
 import net.thucydides.core.annotations.WithTagValuesOf;
 import org.junit.Test;
 
 @WithTagValuesOf({"ListingsAndBuildingsTests", "Buildings"})
-public class BT4_CurrentListingSearchByStatusTest extends SampleTest {
+public class BT6_CurrentListingsLeastExpensiveSortingTest extends SampleTest {
 
     @Test
-    public void currentListingSearchByStatus() {
+    public void currentListingsLeastExpensiveSorting() {
         user.atLoginPage.loginAsBroker(AppProperties.INSTANCE.getProperty("email"),
                 AppProperties.INSTANCE.getProperty("password"));
         user.atPerchwellPage.skipAllHints();
@@ -22,17 +21,15 @@ public class BT4_CurrentListingSearchByStatusTest extends SampleTest {
         user.atSearchPage.clickOnTestBuilding();
         user.atOpenedBuildingPage.fixListingsSearchField();
         user.atOpenedBuildingPage.swipeToTheListings();
-        user.atOpenedBuildingPage.getBedsAndBathsListingsAmount();
-        user.atOpenedBuildingPage.fillInSearchFieldByFilter(Filters.ACTIVE_FILTER);
-        user.atOpenedBuildingPage.checkIfListingsAreFilteredByActiveStatus();
-        user.atOpenedBuildingPage.clickOnClearFieldButton();
-        user.atOpenedBuildingPage.fillInSearchFieldByFilter(Filters.IN_CONTRACT_FILTER);
-        user.atOpenedBuildingPage.checkIfListingsAreFilteredByInContractStatus();
+        user.atOpenedBuildingPage.clickOnSortButton();
+        user.atOpenedBuildingPage.clickOnLeastExpensiveButton();
+        user.atPerchwellPage.shouldListingBeSortedByLeastExpensive();
         user.atOpenedBuildingPage.clickOnPastListingsSection();
-        user.atOpenedBuildingPage.checkIfSearchFieldIsFilledByFilter(Filters.IN_CONTRACT_FILTER);
-        user.atOpenedBuildingPage.checkIfListingsAreFilteredByInContractStatus();
+//        user.atOpenedBuildingPage.clickOnSortButton();
+//        user.atOpenedBuildingPage.checkIfLeastExpensiveSortingButtonIsEnabled();
+//        user.atOpenedBuildingPage.clickOnSortButton();
+        user.atPerchwellPage.shouldListingBeSortedByLeastExpensive();
         user.atOpenedBuildingPage.clickOnCurrentListingsSection();
-        user.atOpenedBuildingPage.clickOnClearFieldButton();
-        user.atOpenedBuildingPage.checkIfListingReturnedToInitialState();
+        user.atPerchwellPage.shouldListingBeSortedByLeastExpensive();
     }
 }
