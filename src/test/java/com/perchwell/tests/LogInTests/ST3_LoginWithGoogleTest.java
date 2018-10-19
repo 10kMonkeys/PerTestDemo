ef73e7f4409b502ad16d5f3cbbf37354b7c5890c
10kMonkeys <aliakseiyarmalchuk@gmail.com>
2018-10-19 11:38:49 +0300

    Add to ignore ST3

--- src/test/java/com/perchwell/tests/LogInTests/ST3_LoginWithGoogleTest.java
+++ src/test/java/com/perchwell/tests/LogInTests/ST3_LoginWithGoogleTest.java
@@ -2,11 +2,13 @@ package com.perchwell.tests.LogInTests;
 
 import com.perchwell.SampleTest;
 import net.thucydides.core.annotations.WithTagValuesOf;
+import org.junit.Ignore;
 import org.junit.Test;
 
 @WithTagValuesOf({"SmokeTestSuit", "LogInTests", "Fourth"})
 public class ST3_LoginWithGoogleTest extends SampleTest {
 
+	@Ignore
 	@Test
 	public void logInWithGoogle() {
 		user.atLoginPage.loginWithGoogle();
