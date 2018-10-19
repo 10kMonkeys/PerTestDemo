a1be6c867bee6b3da669ad056397524ff66f960a
Yauheni.Shautsou <shevtsovea97@gmail.com>
2018-10-19 17:10:19 +0300

    Code refactoring.

---
+++ src/test/java/com/perchwell/tests/AnalyticsTests/AT15_AddACRISClosingMedianChartsTest.java
@@ -0,0 +1,26 @@
+package com.perchwell.tests.AnalyticsTests;
+
+import com.perchwell.SampleTest;
+import com.perchwell.entity.AppProperties;
+import net.thucydides.core.annotations.WithTagValuesOf;
+import org.junit.Test;
+
+@WithTagValuesOf({"AnalyticsTestSuit", "SecondAdditionalAnalytics"})
+public class AT15_AddACRISClosingMedianChartsTest extends SampleTest {
+
+    @Test
+    public void addACRISClosingMedianChartsTest() throws Exception {
+        user.atLoginPage.loginAsBroker(AppProperties.INSTANCE.getProperty("email"),
+                AppProperties.INSTANCE.getProperty("password"));
+        user.atPerchwellPage.skipAllHints();
+        user.atPerchPopup.clickNotNowButton();
+        user.atPerchwellPage.clickOnAnalytics();
+        user.atAnalyticsPage.selectACRISClosingSection();
+        user.atAnalyticsPage.medianButtonClick();
+        user.atAcrisClosingPage.addPriceChart();
+        user.atAnalyticsPage.skipHints();
+        user.atAcrisClosingPage.shouldSeeMedianPriceChart();
+        user.atAnalyticsPage.selectACRISClosingWithMedian();
+        user.atAcrisClosingPage.addAndVerifyMedianPricePerFT2Chart();
+    }
+}
