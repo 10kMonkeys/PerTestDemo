4ce45fafde2e1716d0adc847f1ca83695e4be096
Yauheni.Shautsou <shevtsovea97@gmail.com>
2018-11-12 13:07:18 +0300

    Code refactoring.

--- src/test/java/com/perchwell/pages/clientdetails/ClientPage.java
+++ src/test/java/com/perchwell/pages/clientdetails/ClientPage.java
@@ -89,15 +89,19 @@ public class ClientPage extends BasePage {
 	@iOSXCUITFindBy(iOSClassChain = "**/XCUIElementTypeCell/XCUIElementTypeButton")
 	private WebElement deleteButton;
 
+	@AndroidFindBy(id = "com.perchwell.re.staging:id/search_src_text")
 	@iOSXCUITFindBy(accessibility = "ContactsSearchBar")
 	private WebElement contactsSearchBar;
 
+	@AndroidFindBy(accessibility = "Clear query")
 	@iOSXCUITFindBy(accessibility = "Clear text")
 	private WebElement clearTextButton;
 
+	@AndroidFindBy(accessibility = "CLIENT TEST+CLIENT-TH-CHARTS")
 	@iOSXCUITFindBy(accessibility = "CLIENT TEST+CLIENT-TH-CHARTS")
 	private WebElement testClient;
 
+	@AndroidFindBy(xpath = "//android.widget.RelativeLayout/android.view.ViewGroup/android.widget.TextView[@text != 'DELETE']")
 	@iOSXCUITFindBy(iOSClassChain = "**/XCUIElementTypeTable[`visible==1`]/XCUIElementTypeCell")
 	private List<WebElement> clients;
 
@@ -314,7 +318,12 @@ public class ClientPage extends BasePage {
 	}
 
 	public void swipeCreatedClientName(String name) throws Exception {
-		WebElement client = getDriver().findElement(MobileBy.AccessibilityId(name));
+		WebElement client;
+		if (Config.isAndroid()) {
+			client = element(MobileBy.xpath("//*[@content-desc = '" + name + " ']"));
+		} else {
+			client = element(MobileBy.AccessibilityId(name));
+		}
 		int y = client.getLocation().getY();
 		Helper.universalHorizontalSwipe(client, y+1);
 	}
@@ -325,10 +334,15 @@ public class ClientPage extends BasePage {
 
 	public void enterValueInSearchField(String text) {
 		element(contactsSearchBar).sendKeys(text);
+		if (Config.isAndroid()) {
+			element(contactsSearchBar).click();
+		}
 	}
 
 	public void clickOutsideSearchField() {
-		element(searchFrozenArea).click();
+		if (!Config.isAndroid()) {
+			element(searchFrozenArea).click();
+		}
 	}
 
 	public void clickOnClearTextButton() {
@@ -347,8 +361,14 @@ public class ClientPage extends BasePage {
 	public void isTestClientPresent() {
 		boolean onlyTestClientDisplayed = false;
 
-		if (((listOfClientsAndListings.size() - 20 == 1) && element(testClient).isDisplayed())) {
-			onlyTestClientDisplayed = true;
+		if (Config.isAndroid()) {
+			if (clients.size() == 1 && element(testClient).isDisplayed()) {
+				onlyTestClientDisplayed = true;
+			}
+		} else {
+			if (((listOfClientsAndListings.size() - 20 == 1) && element(testClient).isDisplayed())) {
+				onlyTestClientDisplayed = true;
+			}
 		}
 		Assert.assertTrue(onlyTestClientDisplayed);
 	}
