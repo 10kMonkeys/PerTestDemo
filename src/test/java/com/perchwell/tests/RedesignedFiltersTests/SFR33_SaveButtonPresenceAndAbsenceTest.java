ea4fb185e543255fd60a8853492ea5d113f8462a
Yauheni.Shautsou <shevtsovea97@gmail.com>
2018-12-19 09:48:18 +0300

    Code refactoring.

--- src/test/java/com/perchwell/tests/RedesignedFiltersTests/SFR33_SaveButtonPresenceAndAbsenceTest.java
+++ src/test/java/com/perchwell/tests/RedesignedFiltersTests/SFR33_SaveButtonPresenceAndAbsenceTest.java
@@ -1,4 +1,4 @@
-package com.perchwell.tests.FiltersTests.RedesignedFiltersTests;
+package com.perchwell.tests.RedesignedFiltersTests;
 
 import com.perchwell.SampleTest;
 import com.perchwell.entity.AppProperties;
