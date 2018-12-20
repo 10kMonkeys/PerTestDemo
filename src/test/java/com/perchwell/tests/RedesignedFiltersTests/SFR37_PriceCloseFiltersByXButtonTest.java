280626e77b642f70c2cedd74a56489f738792251
Anton Zenkevich <antonzenkevich1@gmail.com>
2018-12-20 10:48:38 +0300

    SFR37 test updated.

--- src/test/java/com/perchwell/tests/RedesignedFiltersTests/SFR37_PriceCloseFiltersByXButtonTest.java
+++ src/test/java/com/perchwell/tests/RedesignedFiltersTests/SFR37_PriceCloseFiltersByXButtonTest.java
@@ -40,8 +40,8 @@ public class SFR37_PriceCloseFiltersByXButtonTest extends SampleTest {
         user.atSearchPage.clickOnSaveButton();
         user.inSearchMenu.openSearchPage();
         user.atSearchPage.clearMinimumAndMaximumPriceFilters();
-        user.atSearchPage.setMinimumPriceFilter("1000");
-        user.atSearchPage.setMaximumPriceFilter("2000");
+        user.atSearchPage.setMinimumPriceFilter("100000");
+        user.atSearchPage.setMaximumPriceFilter("500000");
         user.atSearchPage.closeSearch();
         user.atPerchwellPage.checkListingsQuantity();
         user.inSearchMenu.openSearchPage();
