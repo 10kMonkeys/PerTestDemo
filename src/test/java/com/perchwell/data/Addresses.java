ba3038fd65d230f362d08c2d43d8c4e6f96f1076
10kMonkeys <aliakseiyarmalchuk@gmail.com>
2019-01-30 11:15:27 +0300

    Update Addresses

--- src/test/java/com/perchwell/data/Addresses.java
+++ src/test/java/com/perchwell/data/Addresses.java
@@ -2,4 +2,7 @@ package com.perchwell.data;
 
 public class Addresses {
     public static final String EAST_35TH = "240 EAST 35TH ST";
+    public static final String DAKOTA = "DAKOTA"; //, 1 WEST 72ND STREET
+    public static final String WALKER_TOWER = "WALKER TOWER"; //, 212 WEST 18TH STREET
+    public static final String UNITED_NAT_PLAZA = "50 UNITED NATIONS PLAZA";
 }
