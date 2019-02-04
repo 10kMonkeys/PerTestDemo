f0da6352e1ddd0e240f9222771a4d2db6607da4d
Anton Ziankevich <anton.ziankevich@itechart-group.com>
2019-02-04 13:16:56 +0300

    TI19 test implemented. A.

---
+++ src/test/java/com/perchwell/tests/TagsTests/TI19_TagWithClientTagDiscussTest.java
@@ -0,0 +1,31 @@
+package com.perchwell.tests.TagsTests;
+
+import com.perchwell.SampleTest;
+import com.perchwell.data.Addresses;
+import com.perchwell.data.DiscussionMessages;
+import com.perchwell.entity.AppProperties;
+import org.junit.Test;
+
+public class TI19_TagWithClientTagDiscussTest extends SampleTest {
+
+    @Test
+    public void tagWithClientTagDiscuss() throws Exception {
+        user.atLoginPage.loginAsBroker(AppProperties.INSTANCE.getProperty("email"),
+                AppProperties.INSTANCE.getProperty("password"));
+        user.atPerchwellPage.skipAllHints();
+        user.atPerchPopup.clickNotNowButton();
+        user.inSearchMenu.openAddressSearch();
+        user.atSearchPage.fillInAddressSearchField(Addresses.EAST_35TH);
+        user.atPerchwellPage.openFirstBuilding();
+        user.atOpenedBuildingPage.clearTagsList();
+        user.atOpenedBuildingPage.clickShareButton();
+        user.atOpenedBuildingPage.clickOnSendWithinPerchwell();
+        user.atDiscussThisListingPage.deleteDiscussionWithClientIfExist();
+        user.usingComplexSteps.sendMessageToClientAndCloseDiscussion(DiscussionMessages.I_D_LIKE_TO_DISCUSS_WITH_YOU);
+        user.atDiscussionsListPage.shouldFindDiscussionsEmailForExistingClient();
+        user.atOpenedBuildingPage.clickOnCancelButtonInDiscussWithClient();
+        user.atOpenedBuildingPage.clickOnArrowBackButtonFromListing();
+        user.atPerchwellPage.openFirstBuilding();
+        user.atOpenedBuildingPage.shouldSeeSpecificClient();
+    }
+}
