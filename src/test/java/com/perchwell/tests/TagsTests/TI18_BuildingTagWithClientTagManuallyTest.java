939d26fc95d6ccdf450bebbbd218990461207ff1
Yauheni.Shautsou <shevtsovea97@gmail.com>
2019-02-04 12:57:10 +0300

    TI18 test implemented. Y.

---
+++ src/test/java/com/perchwell/tests/TagsTests/TI18_BuildingTagWithClientTagManuallyTest.java
@@ -0,0 +1,39 @@
+package com.perchwell.tests.TagsTests;
+
+import com.perchwell.SampleTest;
+import com.perchwell.data.Addresses;
+import com.perchwell.data.SearchRequests;
+import com.perchwell.entity.AppProperties;
+import org.junit.Test;
+
+public class TI18_BuildingTagWithClientTagManuallyTest extends SampleTest {
+
+    @Test
+    public void buildingTagWithClientTagManually() {
+        user.atLoginPage.loginAsBroker(AppProperties.INSTANCE.getProperty("email"),
+                AppProperties.INSTANCE.getProperty("password"));
+        user.atPerchwellPage.skipAllHints();
+        user.atPerchPopup.clickNotNowButton();
+        user.inSearchMenu.openAddressSearch();
+        user.atSearchPage.fillInAddressSearchField(Addresses.EAST_35TH);
+        user.atSearchPage.clickOnTestBuilding();
+        user.atOpenedBuildingPage.getFifthBuildingAddress();
+        user.atOpenedBuildingPage.clearTagsList();
+        user.atOpenedBuildingPage.clickOnMyTagsLabel();
+        user.atTagsPage.fillInTagSearchField(SearchRequests.CLIENT0_TEST);
+        user.atTagsPage.getTestClientsItemsValue();
+        user.atTagsPage.clickOnFirstClientTag();
+        user.atTagsPage.fillInTagSearchField(SearchRequests.CLIENT0_TEST);
+        user.atTagsPage.checkIfClientTagsItemsListIsChanged(1);
+        user.atTagsPage.clickOnArrowBackFromTagsButton();
+        user.atOpenedBuildingPage.shouldSeeFirstExistingTag();
+        user.atOpenedBuildingPage.clickOnArrowBackButtonFromListing();
+        user.atSearchPage.clickOnCrossBackButtonFromAddressSearch();
+        user.atPerchwellPage.clickOnOpenAccountButton();
+        user.atAccountPage.clickOnTagsLabel();
+        user.atTagsPage.fillInTagSearchField(SearchRequests.CLIENT0_TEST);
+        user.atTagsPage.clickOnFirstClientTag();
+        user.atTagsPage.clickOnSearchButton();
+        user.atMyTagsPage.shouldSeeTaggedBuilding();
+    }
+}
