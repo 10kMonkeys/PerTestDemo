74c44ec471f9987daedbfe79b47f5bb9b3656de2
Yauheni.Shautsou <shevtsovea97@gmail.com>
2019-02-21 16:26:19 +0300

    Code refactoring. Y.

--- src/test/java/com/perchwell/pages/tags/TagsPage.java
+++ src/test/java/com/perchwell/pages/tags/TagsPage.java
@@ -375,7 +375,7 @@ public class TagsPage extends TechHelper {
 
 	public void checkIfTagPillIsRemoved(String tagName) {
 		setImplicitTimeout(5, SECONDS);
-		element(MobileBy.AccessibilityId("Tag view: " + tagName)).shouldNotBeVisible();
+		element(MobileBy.iOSClassChain("**/XCUIElementTypeOther[$name BEGINSWITH 'tag color: #'$]/XCUIElementTypeOther/XCUIElementTypeStaticText[$name='" + tagName + "'$]")).shouldNotBeVisible();
 		resetImplicitTimeout();
 	}
 
@@ -501,7 +501,7 @@ public class TagsPage extends TechHelper {
 
 	public void shouldNotSeeSpecificTagsPill(String tagName) {
 		setImplicitTimeout(3, SECONDS);
-		element(MobileBy.AccessibilityId("Tag view: " + tagName)).shouldNotBeVisible();
+		element(MobileBy.iOSClassChain("**/XCUIElementTypeOther[$name BEGINSWITH 'tag color: #'$]/XCUIElementTypeOther/XCUIElementTypeStaticText[$name='" + tagName + "'$]")).shouldNotBeVisible();
 		resetImplicitTimeout();
 	}
 
