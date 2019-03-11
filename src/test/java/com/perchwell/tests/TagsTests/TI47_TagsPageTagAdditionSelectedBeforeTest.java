942a2827d1e69d586717da3bc6750c301e909f61
Anton Zenkevich <anton.ziankevich@itechart-group.com>
2019-03-11 12:03:02 +0300

    TI47 test implemented. A.

---
+++ src/test/java/com/perchwell/tests/TagsTests/TI47_TagsPageTagAdditionSelectedBeforeTest.java
@@ -0,0 +1,70 @@
+package com.perchwell.tests.TagsTests;
+
+import com.perchwell.SampleTest;
+import com.perchwell.entity.AppProperties;
+import org.junit.Test;
+
+public class TI47_TagsPageTagAdditionSelectedBeforeTest extends SampleTest {
+
+    @Test
+    public void tagsPageTagAdditionNotSelectedBeforeTest() {
+        user.atLoginPage.loginAsBroker(AppProperties.INSTANCE.getProperty("email"),
+                AppProperties.INSTANCE.getProperty("password"));
+        user.atPerchwellPage.skipAllHints();
+        user.atPerchPopup.clickNotNowButton();
+        user.atPerchwellPage.clickOnTagIconOnFirstListing();
+        user.atTagsPage.removeAllTagPills();
+        user.atTagsPage.clickOnFirstCustomClientTagAndGetValue();
+        user.atTagsPage.clickOnArrowBackFromTagsButton();
+        user.atPerchwellPage.clickOnTagIconOnSecondListing();
+        user.atTagsPage.removeAllTagPills();
+        user.atTagsPage.fillInTagSearchField("TAGNAME");
+        user.atTagsPage.clickOnFirstTagAndGetValue();
+        user.atTagsPage.clickOnArrowBackFromTagsButton();
+        user.atPerchwellPage.clickOnOpenAccountButton();
+        user.atAccountPage.clickOnTagsLabel();
+        user.atTagsPage.searchCustomClientTag();
+        user.atTagsPage.clickOnFirstTag();
+        user.atTagsPage.clearSearchField();
+        user.atTagsPage.searchFirstExistingTag();
+        user.atTagsPage.clickOnFirstTag();
+        user.atTagsPage.clickOnSearchButton();
+        user.atMyTagsPage.selectFirstListing();
+        user.atMyTagsPage.selectSecondListingAfterFirst();
+        user.atMyTagsPage.clickOnMoreOptionsButton();
+        user.atMyTagsPage.clickOnTagSelectedListingsOption();
+        user.atTagsPage.shouldSeeCustomClientTagsPill();
+        user.atTagsPage.shouldSeeFirstExistingTagsPill();
+        user.atTagsPage.searchFirstExistingTag();
+        user.atTagsPage.allTagsAreSelected(1);
+        user.atTagsPage.clearSearchField();
+        user.atTagsPage.searchCustomClientTag();
+        user.atTagsPage.allTagsAreSelected(1);
+        user.atTagsPage.getFirstTagsItemsValue();
+        user.atTagsPage.clickOnFirstTag();
+        user.atTagsPage.checkIfCustomClientTagsItemsListIsChanged(-1);
+        user.atTagsPage.getFirstTagsItemsValue();
+        user.atTagsPage.clickOnFirstTag();
+        user.atTagsPage.checkIfCustomClientTagsItemsListIsChanged(2);
+        user.atTagsPage.clickOnArrowBackFromTagsButton();
+        user.atMyTagsPage.checkTagsIconCountForFirstListing("1");
+        user.atMyTagsPage.checkTagsIconCountForSecondListing("2");
+        user.atMyTagsPage.clickOnTagIconOnFirstListing();
+        user.atTagsPage.shouldSeeCustomClientTagsPill();
+        user.atTagsPage.clickOnArrowBackFromTagsButton();
+        user.atMyTagsPage.openFirstBuilding();
+        user.atOpenedBuildingPage.skipDiscussWithClientHint();
+        user.atOpenedBuildingPage.shouldSeeCustomClientTag();
+        user.atOpenedBuildingPage.clickOnMyTagsLabel();
+        user.atTagsPage.shouldSeeCustomClientTagsPill();
+        user.atTagsPage.clickOnArrowBackFromTagsButton();
+        user.atOpenedBuildingPage.clickOnArrowBackButtonFromListing();
+        user.atMyTagsPage.clickOnTagIconOnSecondListing();
+        user.atTagsPage.shouldSeeCustomClientTagsPill();
+        user.atTagsPage.clickOnArrowBackFromTagsButton();
+        user.atMyTagsPage.openSecondBuilding();
+        user.atOpenedBuildingPage.shouldSeeCustomClientTag();
+        user.atOpenedBuildingPage.clickOnMyTagsLabel();
+        user.atTagsPage.shouldSeeCustomClientTagsPill();
+    }
+}
