90e7976fcbc59048be3493bfb2ce02a34c582b0f
ICX\Anton.Ziankevich <anton.ziankevich@itechart-group.com>
2019-04-09 11:50:15 +0300

    Add slackMessageSender for Android. A.

--- src/test/java/com/perchwell/helpers/SlackMessageSender.java
+++ src/test/java/com/perchwell/helpers/SlackMessageSender.java
@@ -1,16 +1,25 @@
 package com.perchwell.helpers;
 
+import com.perchwell.crossPlatform.Config;
 import net.gpedro.integrations.slack.SlackApi;
 import net.gpedro.integrations.slack.SlackMessage;
 
 public class SlackMessageSender {
 
+    //https://hooks.slack.com/services/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 - Android
     //https://hooks.slack.com/services/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 - iOS
     //https://hooks.slack.com/services/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 - MyTest
 
     public void sendMessage(String slackTestFailedMessage) {
+        SlackApi api;
 
-        SlackApi api = new SlackApi("https://hooks.slack.com/services/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");
-        api.call(new SlackMessage("#ios_tests", "Perchwell Bot", slackTestFailedMessage));
+        if(Config.isAndroid()) {
+            api = new SlackApi("https://hooks.slack.com/services/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");
+            api.call(new SlackMessage("#android_tests", "Perchwell Bot", slackTestFailedMessage));
+        }
+        else {
+            api = new SlackApi("https://hooks.slack.com/services/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");
+            api.call(new SlackMessage("#ios_tests", "Perchwell Bot", slackTestFailedMessage));
+        }
     }
 }
