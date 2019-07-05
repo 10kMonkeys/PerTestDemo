6cc2a0a30a8b9821d082df0f9386bc2b3c317830
ICX\Anton.Ziankevich <anton.ziankevich@itechart-group.com>
2019-07-05 10:09:23 +0300

    LVT10 test updated. A.

--- src/test/java/com/perchwell/tests/ListViewTests/LVT10_MultiSelectDismissalTest.java
+++ src/test/java/com/perchwell/tests/ListViewTests/LVT10_MultiSelectDismissalTest.java
@@ -19,20 +19,17 @@ public class LVT10_MultiSelectDismissalTest extends SampleTest {
         user.atSearchPage.setSearchName();
         user.atSearchPage.clickOnSaveButton();
         user.usingComplexSteps.startNewSearch();
-        user.onlyAndroid.resetListView(); //
         user.atPerchwellPage.selectFirstListing();
         user.inSearchMenu.openSavedSearches();
         user.atSearchPage.clickOnPreviouslyCreatedSearch();
         user.atPerchwellPage.checkNoOneListingIsSelected();
         user.atPerchwellPage.multiSelectMenuIsHidden();
         user.atPerchwellPage.checkSortLabel(SortingTypes.NEWEST);
-        user.onlyAndroid.resetListView(); //
         user.atPerchwellPage.selectFirstListing();
         user.usingComplexSteps.startNewSearch();
         user.atPerchwellPage.checkNoOneListingIsSelected();
         user.atPerchwellPage.multiSelectMenuIsHidden();
         user.atPerchwellPage.checkSortLabel(SortingTypes.NEWEST);
-        user.onlyAndroid.resetListView(); //
         user.atPerchwellPage.selectFirstListing();
         user.inSearchMenu.openSearchPage();
         user.atSearchPage.clickOnResetFilter();
@@ -40,14 +37,12 @@ public class LVT10_MultiSelectDismissalTest extends SampleTest {
         user.atPerchwellPage.checkNoOneListingIsSelected();
         user.atPerchwellPage.multiSelectMenuIsHidden();
         user.atPerchwellPage.checkSortLabel(SortingTypes.NEWEST);
-        user.onlyAndroid.resetListView(); //
         user.atPerchwellPage.selectFirstListing();
         user.inSearchMenu.openSearchPage();
         user.atSearchPage.clickOnApplyButton();
         user.atPerchwellPage.checkNoOneListingIsSelected();
         user.atPerchwellPage.multiSelectMenuIsHidden();
         user.atPerchwellPage.checkSortLabel(SortingTypes.NEWEST);
-        user.onlyAndroid.resetListView(); //
         user.atPerchwellPage.selectFirstListing();
         user.atPerchwellPage.swipeUpListViewToRefresh();
         user.atPerchwellPage.checkNoOneListingIsSelected();
