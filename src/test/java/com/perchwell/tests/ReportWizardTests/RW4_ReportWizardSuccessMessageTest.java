d15a6efcebd3d02336e97b49dc3b3e105a6e6200
Pavel Kozlov <darkdummoknight@gmail.com>
2019-07-17 11:09:58 +0300

    rw4 quick fix

--- src/test/java/com/perchwell/tests/ReportWizardTests/RW4_ReportWizardSuccessMessageTest.java
+++ src/test/java/com/perchwell/tests/ReportWizardTests/RW4_ReportWizardSuccessMessageTest.java
@@ -24,6 +24,9 @@ public class RW4_ReportWizardSuccessMessageTest extends SampleTest {
         user.atCreateReportPage.fillEmailField("report-valid-wizard1@email.com");
         user.atCreateReportPage.fillSubjectField(DiscussionMessages.REPORT_SUBJECT);
         user.atCreateReportPage.clickOnEmailReportButton();
+        user.atCreateReportPage.checkEmailReportPopUpIsShown();
+        user.atCreateReportPage.checkPopUpExitButtonIsShown();
+        user.atCreateReportPage.checkPopUpOkButtonIsShown();
         user.atCreateReportPage.clickOnPopUpOkButton();
         user.atCreateReportPage.checkPopUpMessageIsClosed();
         user.atCreateReportPage.checkEmailFieldIsShownAsGreenPill();
