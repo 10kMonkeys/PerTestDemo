87fa9902f63b9f1195fda829e8d65c23fd6706aa
Pavel Kozlov <darkdummoknight@gmail.com>
2019-09-13 16:04:37 +0300

    reset filters locator update

--- src/test/java/com/perchwell/pages/perchwell/SearchPage.java
+++ src/test/java/com/perchwell/pages/perchwell/SearchPage.java
@@ -155,7 +155,7 @@ public class SearchPage extends TechHelper {
     private WebElement createNewSearchButton;
 
     @AndroidFindBy(id = "com.perchwell.re.staging:id/reset_filters")
-    @iOSXCUITFindBy(accessibility = "Reset Search Filters")
+    @iOSXCUITFindBy(iOSNsPredicate = "type == 'XCUIElementTypeOther' AND name CONTAINS 'Reset Filters'")
     private WebElement resetFiltersButton;
 
     @AndroidFindBy(accessibility = "Bedrooms: 1")
